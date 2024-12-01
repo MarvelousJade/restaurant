61f0f7527405a31bdb846e32f3546134742ed820
shaoyu <fanshaoyu9@outlook.com>
2024-11-30 19:31:34 -0500

    Fix deallocate for m_name in Billable

--- ms1/Billable.cpp
+++ ms1/Billable.cpp
@@ -2,7 +2,9 @@
 #include "Utils.h"
 namespace seneca {
 void Billable::setPrice(double price) { m_price = price; }
-void Billable::setName(const char *name) { m_name = ut.alocpy(name); }
+void Billable::setName(const char *name) {
+  ut.alocpy(m_name, name);
+}
 Billable::Billable() {
   m_price = 0.0;
   m_name = nullptr;
@@ -10,18 +12,18 @@ Billable::Billable() {
 Billable::Billable(Billable &source) {
   m_name = nullptr;
   m_price = source.m_price;
-  m_name = ut.alocpy(source.m_name);
+  ut.alocpy(m_name, source.m_name);
 }
 Billable &Billable::operator=(const Billable &source) {
   if (this != &source) {
     m_price = source.m_price;
-    m_name = ut.alocpy(source.m_name);
+    ut.alocpy(m_name, source.m_name);
   }
   return *this;
 }
 Billable::~Billable() {
   m_price = 0.0;
-  delete m_name;
+  delete[] m_name;
 }
 const char* Billable::name() const { return m_name; }
 double Billable::price() const { return m_price; }
